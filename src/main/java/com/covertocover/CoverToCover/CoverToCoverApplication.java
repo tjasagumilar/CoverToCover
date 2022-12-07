87c1b4d54a57f7aa6a857f19d5ac507dd3f5ccfd
tjasag <tjasa.gumilar@student.um.si>
2022-12-07 19:41:42 +0100

    Initial commit

---
+++ src/main/java/com/covertocover/CoverToCover/CoverToCoverApplication.java
@@ -0,0 +1,13 @@
+package com.covertocover.CoverToCover;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoverToCoverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoverToCoverApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/covertocover/CoverToCover/CoverToCoverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.covertocover.CoverToCover;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoverToCoverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
