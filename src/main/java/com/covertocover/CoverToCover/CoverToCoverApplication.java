b1835b9a684e3eb6fb4fbb612bd11bb02f2e8d03
miha_plemenitas <miha.plemenitas@student.um.si>
2022-12-08 18:13:17 +0100

    vspostavitev baze #CTC-7 DONE Time 1h

--- src/main/java/com/covertocover/CoverToCover/CoverToCoverApplication.java
+++ src/main/java/com/covertocover/CoverToCover/CoverToCoverApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class CoverToCoverApplication {
 
+	//glavno!
 	public static void main(String[] args) {
 		SpringApplication.run(CoverToCoverApplication.class, args);
 	}
