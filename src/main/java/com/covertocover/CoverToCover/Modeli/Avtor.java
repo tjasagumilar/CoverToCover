5c9089db26e21f9efbce3766bf497f5cea259cde
tjasag <tjasa.gumilar@student.um.si>
2022-12-08 18:38:45 +0100

    Dodan model in kontroler

---
+++ src/main/java/com/covertocover/CoverToCover/Modeli/Avtor.java
@@ -0,0 +1,9 @@
+package com.covertocover.CoverToCover.Modeli;
+
+import java.util.Vector;
+
+public class Avtor {
+    private String _ime;
+    private String _priimek;
+    public Vector<Knjiga> _ima = new Vector<Knjiga>();
+}
