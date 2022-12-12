ce68dc137be1314ee5d62c71b99a5fa6d50d2478
miha_plemenitas <miha.plemenitas@student.um.si>
2022-12-12 18:04:45 +0100

    Pripravljen razred OcenaKontroler #CTC-34 DONE

---
+++ src/main/java/com/covertocover/CoverToCover/Kontroler/OcenaKontroler.java
@@ -0,0 +1,11 @@
+package com.covertocover.CoverToCover.Kontroler;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/ocena")
+public class OcenaKontroler {
+
+
+}
