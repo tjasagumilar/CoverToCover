b3152cfd13239603f9b9d9312ea1af7af9e6814f
miha_plemenitas <miha.plemenitas@student.um.si>
2022-12-12 18:12:04 +0100

    Metoda GET za razred Ocena #CTC-36 DONE

--- src/main/java/com/covertocover/CoverToCover/Kontroler/OcenaKontroler.java
+++ src/main/java/com/covertocover/CoverToCover/Kontroler/OcenaKontroler.java
@@ -1,11 +1,29 @@
 package com.covertocover.CoverToCover.Kontroler;
 
+import com.covertocover.CoverToCover.Modeli.Ocena;
+import com.covertocover.CoverToCover.Repozitorij.OcenaRepozitorij;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/ocena")
 public class OcenaKontroler {
 
+    @Autowired
+    private OcenaRepozitorij ocenaRep;
 
+    @GetMapping("")
+    public Iterable<Ocena> vrniVseZbirke() {
+        return ocenaRep.findAll();
+    }
+
+    @GetMapping("{id}")
+    public Optional<Ocena> vrniZbirko(@PathVariable(name="id") Long id) {
+        return ocenaRep.findById(id);
+    }
 }
